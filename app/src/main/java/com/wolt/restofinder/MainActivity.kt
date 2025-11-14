cd90c9af2c81d61bcbb6e998c1ff00c464025ce2
Nnaemeka Abah <hello@abahnj.dev>
2025-11-14 14:38:07 +0300

    feat(app): integrate VenueListScreen in MainActivity
      Replaces default Android greeting scaffold with VenueListScreen.

--- app/src/main/java/com/wolt/restofinder/MainActivity.kt
+++ app/src/main/java/com/wolt/restofinder/MainActivity.kt
@@ -4,14 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.wolt.restofinder.presentation.theme.RestoFinderTheme
+import com.wolt.restofinder.presentation.venues.VenueListScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -21,29 +15,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RestoFinderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                VenueListScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RestoFinderTheme {
-        Greeting("Android")
     }
 }
